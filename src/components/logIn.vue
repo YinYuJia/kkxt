6482fba027c5d1b537990876979541983ddf3c4d
zhouhao-dev <851801789@qq.com>
2019-12-09 16:18:34 +0800

    登录修改

--- src/components/logIn.vue
+++ src/components/logIn.vue
@@ -1,35 +1,23 @@
 <!--  -->
 <template>
   <div  class="login-box">
+    <div class="login-left">
+    </div>
     <div class="login-container">
-      <el-form :model="form" label-width="80px" id="FORM" style="margin-top:0.15rem" action="/ApiUrl2/sas/api/login" method="post" :rules="rules" status-icon>
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" name="username" clearable placeholder="请输入用户名"></el-input>
+      <div class="login-header">
+        <p>用户登录</p>
+      </div>
+      <el-form :model="form" label-width="80px" id="FORM" style="margin-top:0.15rem" action="/ApiUrl2/sas/api/login" method="post">
+      <el-form-item label="用户名" prop="username" hide-required-asterisk>
+        <el-input v-model="form.username" name="username" clearable placeholder="请输入用户名" suffix-icon="el-icon-date"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="passwoed">
-        <el-input v-model="form.password" name="password" style="width:1.2rem" clearable placeholder="请输入密码"></el-input>
+        <el-input v-model="form.password" name="password" clearable placeholder="请输入密码"></el-input>
       </el-form-item>
-      <el-button type="primary" @click="submitForm" class="btn">立即创建</el-button>
-      <el-button @click="resetForm">重置</el-button>
+      <el-button type="submit" class="btn">立即登录</el-button>
     </el-form>
     </div>
   </div>
-  <!-- ------------------------------------------------------------------- -->
-   <!-- <div class="login">
-    <el-form ref="form" :model="form" label-width="80px" :rules="rules" status-icon>
-      <img src="../assets/avatar.jpg" alt />
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="form.password" placeholder="请输入用户密码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login">登录</el-button>
-        <el-button @click="reset">重置</el-button>
-      </el-form-item>
-    </el-form>
-  </div> -->
 </template>
 
 <script>
@@ -40,15 +28,6 @@
         form: {
           username: "",
           password: ""
-        },
-        rules: {
-          name: [
-            { required: true, message: '请输入用户名', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ]
         }
       }
     },
@@ -99,19 +78,6 @@
         }).catch((error) => {
           console.log(error)
         })
-      },
-       submitForm() {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-    },
-    resetForm() {
-        this.$refs[formName].resetFields();
       }
   }
 }
@@ -139,22 +105,64 @@
     background-size: 100% 100%;
     min-height:100vh;
     overflow: hidden;
+    position: relative;
+    .login-left {
+      width: 12.62rem;
+      height:9.44rem;
+      background: url('../assets/imgs/login_center.png') no-repeat;
+      background-size: 100% 100%;
+      position: absolute;
+      left: 0px;
+      top: 0px;
+    }
     .login-container {
       width: 5rem;
       height: 8rem;
-      background-color: #fff;
+      // background-color: #fff;
       float: right;
       margin-top: .9rem;
       margin-right: .5rem;
-      padding: .2rem;
+      padding-right: .45rem;
+      padding-top: .32rem;
+      padding-left: .32rem;
+      box-sizing: border-box;
+      background: url('../assets/imgs/login_form.png') no-repeat;
+      .login-header {
+        width: 100%;
+        height: .68rem;
+        padding-top: 15px;
+        margin-bottom: .45rem;
+        p {
+          display: block;
+          text-align: center;
+          margin: 0 auto;
+          font-size: .25rem;
+          color: #fff;
+          width: 1.6rem;
+          height: .53rem;
+          border-bottom: .01rem solid #1890ff;
+          padding-top: .1rem;
+          box-sizing: border-box;
+        }
+      }
     }
   }
   .btn {
+    border: none;
     height: .5rem;
-    width: 1.2rem;
+    width: 3.3927rem;
     font-size: .16rem;
-    background-color: red;
+    background-color: #1890ff;
     color: #fff;
     border-radius: 5px;
+    float: right;
+    margin-right: .02rem;
+  }
+  /deep/ .el-form-item__label {
+        font-size: .2rem;
+        color: #fff;
+      }
+  /deep/ .el-form-item {
+    margin-bottom: .5rem;
   }
 </style>
